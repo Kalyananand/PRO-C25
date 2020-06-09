3ad07d351a84e299bd17b56773eb1d390f0300d9
Kalyananand <58302840+Kalyananand@users.noreply.github.com>
2020-06-09 10:52:19 +0530

    Add files via upload

---
+++ raindrop.js
@@ -0,0 +1,12 @@
+class Raindrop {
+ constructor(x,y,w,h){
+     var options = {
+         'restituition':0.8,
+         'friction':0,
+         'density':0
+     }
+     this.width = width;
+     this.height = height;
+     World.add(world,this.body);
+ }   
+}
